ef9ffc8cde1f9ea582dba4e0bae98c52708ac3f8
VictorLira-DEV <victorliradev@gmail.com>
2021-12-05 18:53:07 -0200

    working with grid interface

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "../styles/components/Loading/Loading.module.css";
 
 const Loading = () => {
-    return <div className={styles.circle}></div>;
+    return (
+        <div className={styles["circle-wrapper"]}> 
+            <div className={styles.circle}></div>
+        </div>
+    );
 };
 
 export default Loading;
