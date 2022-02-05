7e745a3dabfe3b5d3c2dd0885417928908bae4dd
VictorLira-DEV <victorliradev@gmail.com>
2022-02-05 13:13:35 -0300

    adding  authorization

--- src/services/api/lotr.ts
+++ src/services/api/lotr.ts
@@ -3,5 +3,5 @@ export const URLcharactersRace = "https://the-one-api.dev/v2/character?race=";
 
 export const headers = {
     Accept: "application/json",
-    Authorization: "Bearer Mx9pH2p0_osox9nIk9Tw",
+    Authorization: "Bearer NxvOQmZ4tS-ZAljD5DFs",
 };
